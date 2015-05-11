34336b8bdd508d42294cdb5688af82722f4eb07c
aaronm <aaronmintz@AARONs-MBP.SemenkoWasHere>
2015-05-11 17:57:58 -0500

    added models to admin

--- logs/admin.py
+++ logs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Trainee, Attending, Procedure, Procedure_Code, Patient
+
+admin.site.register(Trainee)
+admin.site.register(Attending)
+admin.site.register(Procedure)
+admin.site.register(Procedure_Code)
+admin.site.register(Patient)
 
 # Register your models here.
